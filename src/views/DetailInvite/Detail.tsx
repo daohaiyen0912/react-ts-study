46840f25ea88994434b96b671a2a14cde1b68a8b
daohaiyen0912 <daohaiyen0912@gmail.com>
2022-07-25 09:07:38 +0700

    fix bug warning

--- src/views/DetailInvite/Detail.tsx
+++ src/views/DetailInvite/Detail.tsx
@@ -30,6 +30,10 @@ export const Detail = () => {
         
     }
 
+    const stChange = () => {
+
+    }
+
     return (
         <>
             <Admin />
@@ -55,16 +59,16 @@ export const Detail = () => {
                         <div className="detail-content-1-2">
                             <div>
                                 <label htmlFor="DT_001">Mã đấu thầu</label>
-                                <input value="DT_001" id="DT_001"></input>
+                                <input value="DT_001" id="DT_001" onChange={stChange}></input>
                             </div>
                             <div>
                                 <label htmlFor="HSMT_001">Mã HSMT</label>
-                                <input value="HSMT_001" id="HSMT_001"></input>
+                                <input value="HSMT_001" id="HSMT_001" onChange={stChange}></input>
                             </div>
                         </div>
                         <div className="detail-content-1-3">
                             <label htmlFor="mua-sam-an-uong">Tên gói thầu</label>
-                            <input value="Gói thầu mua sắm laptop" type="select" id="mua-sam-an-uong"></input>
+                            <input value="Gói thầu mua sắm laptop" type="select" id="mua-sam-an-uong" onChange={stChange}></input>
                         </div>
                         <div className="detail-content-1-4">
                             <div className="detail-content-1-4-1">
@@ -88,26 +92,26 @@ export const Detail = () => {
                                                 Tên
                                                 <span className="note-important">*</span>
                                             </label>
-                                            <input id="ten-ben-mt" placeholder="Hiển thị theo thông tin HSMT"></input>
+                                            <input id="ten-ben-mt" placeholder="Hiển thị theo thông tin HSMT" onChange={stChange}></input>
                                         </div>
                                         <div className="detail-dc-ben-mt">
                                             <label htmlFor="dia-chi-ben-mt">
                                                 Địa chỉ
                                                 <span className="note-important">*</span>
                                             </label>
-                                            <input id="dia-chi-ben-mt" placeholder="Hiển thị theo thông tin HSMT"></input>
+                                            <input id="dia-chi-ben-mt" placeholder="Hiển thị theo thông tin HSMT" onChange={stChange}></input>
                                         </div>
                                         <div className="dien-thoai-ben-mt">
                                             <label htmlFor="dien-thoai-ben-mt">Điện thoại</label>
-                                            <input id="dien-thoai-ben-mt" placeholder="Hiển thị theo thông tin HSMT"></input>
+                                            <input id="dien-thoai-ben-mt" placeholder="Hiển thị theo thông tin HSMT" onChange={stChange}></input>
                                         </div>
                                         <div className="fax-ben-mt">
                                             <label htmlFor="fax-ben-mt">Fax</label>
-                                            <input id="fax-ben-mt" placeholder="Hiển thị theo thông tin HSMT"></input>
+                                            <input id="fax-ben-mt" placeholder="Hiển thị theo thông tin HSMT" onChange={stChange}></input>
                                         </div>
                                         <div className="detail-ms-thue-ben-mt">
                                             <label htmlFor="ms-thue-ben-mt">Mã số thuế</label>
-                                            <input id="ms-thue-ben-mt" placeholder="Hiển thị theo thông tin HSMT"></input>
+                                            <input id="ms-thue-ben-mt" placeholder="Hiển thị theo thông tin HSMT" onChange={stChange}></input>
                                         </div>
                                     </div>
                                 </div>
@@ -119,22 +123,22 @@ export const Detail = () => {
                                                 Tên người phụ trách
                                                 <span className="note-important">*</span>
                                             </label>
-                                            <input id="ten-phu-trach" placeholder="Hiển thị theo thông tin HSMT"></input>
+                                            <input id="ten-phu-trach" placeholder="Hiển thị theo thông tin HSMT" onChange={stChange}></input>
                                         </div>
                                         <div className="detail-dia-chi-pt"> 
                                             <label htmlFor="dia-chi-pt">
                                                 Địa chỉ
                                                 <span className="note-important">*</span>
                                             </label>
-                                            <input id="dia-chi-pt" placeholder="Hiển thị theo thông tin HSMT"></input>
+                                            <input id="dia-chi-pt" placeholder="Hiển thị theo thông tin HSMT" onChange={stChange}></input>
                                         </div>
                                         <div className="detail-dt-pt">
                                             <label htmlFor="dt-pt">Điện thoại</label>
-                                            <input id="dt-pt" placeholder="Hiển thị theo thông tin HSMT"></input>
+                                            <input id="dt-pt" placeholder="Hiển thị theo thông tin HSMT" onChange={stChange}></input>
                                         </div>
                                         <div className="detail-fax-pt">
                                             <label htmlFor="fax-pt">Fax</label>
-                                            <input id="fax-pt" placeholder="Hiển thị theo thông tin HSMT"></input>
+                                            <input id="fax-pt" placeholder="Hiển thị theo thông tin HSMT" onChange={stChange}></input>
                                         </div>
                                     </div>
                                 </div>
@@ -147,21 +151,21 @@ export const Detail = () => {
                                                     Thời gian phát hành HSMT
                                                     <span className="note-important">*</span>
                                                 </label>
-                                                <input id="time-phat-hanh" placeholder="Hiển thị theo thông tin HSMT"></input>
+                                                <input id="time-phat-hanh" placeholder="Hiển thị theo thông tin HSMT" onChange={stChange}></input>
                                             </div>
                                             <div className="detail-time-bd-nop-hs can-margin-left">
                                                 <label htmlFor="time-bd-nop-hs">
                                                     Thời gian bắt đầu nộp hồ sơ
                                                     <span className="note-important">*</span>
                                                 </label>
-                                                <input id="time-bd-nop-hs" placeholder="Hiển thị theo thông tin HSMT"></input>
+                                                <input id="time-bd-nop-hs" placeholder="Hiển thị theo thông tin HSMT" onChange={stChange}></input>
                                             </div>
                                             <div className="detail-time-dong-thau can-margin-left">
                                                 <label htmlFor="time-dong-thau">
                                                     Thời điểm đóng thầu
                                                     <span className="note-important">*</span>
                                                 </label>
-                                                <input id="time-dong-thau" placeholder="Hiển thị theo thông tin HSMT"></input>
+                                                <input id="time-dong-thau" placeholder="Hiển thị theo thông tin HSMT" onChange={stChange}></input>
                                             </div>
                                         </div>
                                         <div className="detail-tt-moi-thau-2">
@@ -170,21 +174,21 @@ export const Detail = () => {
                                                     Thời điểm mở thầu
                                                     <span className="note-important">*</span>
                                                 </label>
-                                                <input id="time-mo-thau" placeholder="Hiển thị theo thông tin HSMT"></input>
+                                                <input id="time-mo-thau" placeholder="Hiển thị theo thông tin HSMT" onChange={stChange}></input>
                                             </div>
                                             <div className="detail-time-bd-cham-thau can-margin-left">
                                                 <label htmlFor="time-bd-cham-thau">
                                                     Thời điểm bắt đầu chấm thầu
                                                     <span className="note-important">*</span>
                                                 </label>
-                                                <input id="time-bd-cham-thau" placeholder="Hiển thị theo thông tin HSMT"></input>
+                                                <input id="time-bd-cham-thau" placeholder="Hiển thị theo thông tin HSMT" onChange={stChange}></input>
                                             </div>
                                             <div className="detail-time-kt-cham-thau can-margin-left">
                                                 <label htmlFor="time-kt-cham-thau">
                                                     Thời điểm kết thúc chấm thầu
                                                     <span className="note-important">*</span>
                                                 </label>
-                                                <input id="time-kt-cham-thau" placeholder="Hiển thị theo thông tin HSMT"></input>
+                                                <input id="time-kt-cham-thau" placeholder="Hiển thị theo thông tin HSMT" onChange={stChange}></input>
                                             </div>
                                         </div>
                                     </div>
@@ -197,21 +201,21 @@ export const Detail = () => {
                                                 Hình thức đấu thầu
                                                 <span className="note-important">*</span>
                                             </label>
-                                            <input id="ht-dau-thau" placeholder="Hiển thị theo thông tin HSMT"></input>
+                                            <input id="ht-dau-thau" placeholder="Hiển thị theo thông tin HSMT" onChange={stChange}></input>
                                         </div>
                                         <div className="can-margin-left">
                                             <label htmlFor="ht-nop">
                                                 Hình thức nộp
                                                 <span className="note-important">*</span>
                                             </label>
-                                            <input id="ht-nop" placeholder="Hiển thị theo thông tin HSMT"></input>
+                                            <input id="ht-nop" placeholder="Hiển thị theo thông tin HSMT" onChange={stChange}></input>
                                         </div>
                                         <div className="can-margin-left">
                                             <label htmlFor="pt-lc">
                                                 Phương thức lựa chọn nhà thầu
                                                 <span className="note-important">*</span>
                                             </label>
-                                            <input id="pt-lc" placeholder="Hiển thị theo thông tin HSMT"></input>
+                                            <input id="pt-lc" placeholder="Hiển thị theo thông tin HSMT" onChange={stChange}></input>
                                         </div>
                                     </div>
                                 </div>
